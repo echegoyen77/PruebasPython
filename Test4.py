fcaa677943c5850a95d6d90ad2a6a03fbc7bebd4
echegoyen77 <echegoyen@gmail.com>
2024-10-08 13:03:22 +0200

    Cambios en el programa para calcular el número primo

--- Test4.py
+++ Test4.py
@@ -1,16 +1,20 @@
 #Escribir un programa para verificar si un número es primo o no. 
 
-
-
 def esPrimo(num):
     if (num < 1):
         return False
+    elif num==2:
+        return True
     else:
-        if (num % 2 == 0):
-            return True
-        else: 
-            return False
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
 
 num = int(input("Escriba un número: "))
 resultado = esPrimo(num)
 
+if resultado:
+    print("El número introducido es primo")
+else: 
+    print("El número introducido NO es primo")
